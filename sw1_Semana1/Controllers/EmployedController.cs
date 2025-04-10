aa4a8e3a20e8305230642bbd588036c53ecf88ec
Juan David Anaya Rodríguez <davidanaya051@gmail.com>
2025-04-10 00:18:52 -0500

    ya funciona el registrar

--- sw1_Semana1/Controllers/EmployedController.cs
+++ sw1_Semana1/Controllers/EmployedController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 using System.Web.Mvc;
 using sw1_Semana1.dao;
@@ -11,11 +12,50 @@ namespace sw1_Semana1.Controllers
 {
     public class EmployedController : Controller
     {
-        IEmployedsDao dao = new EmployedDaoImpl(); 
+        IEmployedsDao dao = new EmployedDaoImpl();
         // GET: Employed
         public ActionResult Listar()
         {
             return View(dao.OperacionesLectura("listar", new Employeds()));
         }
+
+        [HttpGet]
+        public ActionResult Crear()
+        {
+            ViewBag.comboDepartament = new SelectList(listaDepartamentos(), "department_id", "department_name");
+            ViewBag.combojobs = new SelectList(listaJobs(), "job_id", "job_title");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Crear(Employeds objEmployeds)
+        {
+            int procesar = dao.operacionesEscritura("Insertar", objEmployeds);
+            ViewBag.comboDepartament = new SelectList(listaDepartamentos(), "department_id", "department_name");
+            ViewBag.combojobs = new SelectList(listaJobs(), "job_id", "job_title");
+            if (procesar >= 0)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+                ViewBag.mensajeError = "Ocurrió un problema al insertar";
+                return View(objEmployeds);
+            }
+        }
+        public List<Department> listaDepartamentos()
+        {
+            IDepartamentDao dao = new DepartamentImpl();
+            List<Department> lista = dao.operacionesEscritura("listar", new Department());
+            return lista;
+        }
+        public List<Jobs> listaJobs()
+        {
+            IJobsDao dao = new JobsDaoImpl();
+            List<Jobs> lista = dao.OperacionesLectura("listar", new Jobs());
+            return lista;
+        }
+
+      
+
     }
 }
--- sw1_Semana1/dao/DaoImpl/EmployedDaoImpl.cs
+++ sw1_Semana1/dao/DaoImpl/EmployedDaoImpl.cs
@@ -19,21 +19,22 @@ namespace sw1_Semana1.dao.DaoImpl
                    
                 {
                     conn.Open();
-                    using (SqlCommand cmd = new SqlCommand("usp_employeds_crud2", conn))
+                    using (SqlCommand cmd = new SqlCommand("usp_employeds_crud3", conn))
                     {
                         cmd.CommandType = System.Data.CommandType.StoredProcedure;
                         cmd.Parameters.Clear();
+                        // Asignación de parámetros a los valores recibidos en el objeto `objEmployeds`
                         cmd.Parameters.AddWithValue("@indicador", indicador);
-                        cmd.Parameters.AddWithValue("@employe_id", objEmployeds.employe_id);
-                        cmd.Parameters.AddWithValue("@first_name", objEmployeds.first_name);
-                        cmd.Parameters.AddWithValue("@last_name", objEmployeds.last_name);
-                        cmd.Parameters.AddWithValue("@email", objEmployeds.email);
-                        cmd.Parameters.AddWithValue("@phone_number", objEmployeds.phone_number);
-                        cmd.Parameters.AddWithValue("@hire_date", objEmployeds.hire_date);
-                        cmd.Parameters.AddWithValue("@job_id", objEmployeds.job_id);
-                        cmd.Parameters.AddWithValue("@salary", objEmployeds.salary);
-                        cmd.Parameters.AddWithValue("@manager_id", objEmployeds.manager_id);
-                        cmd.Parameters.AddWithValue("@departament_id", objEmployeds.department_id);
+                        cmd.Parameters.AddWithValue("@employe_id", objEmployeds.employe_id ?? (object)DBNull.Value); // Asignación de nulos de manera segura
+                        cmd.Parameters.AddWithValue("@first_name", objEmployeds.first_name ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@last_name", objEmployeds.last_name ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@email", objEmployeds.email ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@phone_number", objEmployeds.phone_number ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@hire_date", objEmployeds.hire_date ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@job_id", objEmployeds.job_id ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@salary", objEmployeds.salary.HasValue ? objEmployeds.salary.Value : (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@manager_id", objEmployeds.manager_id ?? (object)DBNull.Value);
+                        cmd.Parameters.AddWithValue("@departament_id", objEmployeds.department_id ?? (object)DBNull.Value);
                         procesar = cmd.ExecuteNonQuery(); // Ejecuta el comando y si funciona redirige a la vista que le corresponde
                     }
                 }
@@ -59,7 +60,7 @@ namespace sw1_Semana1.dao.DaoImpl
                     conn.Open();
 
                     // Comando SQL para ejecutar el procedimiento almacenado 'usp_employeds_crud2'
-                    using (SqlCommand cmd = new SqlCommand("usp_employeds_crud2", conn))
+                    using (SqlCommand cmd = new SqlCommand("usp_employeds_crud3", conn))
                     {
                         // Definimos el tipo de comando como procedimiento almacenado
                         cmd.CommandType = System.Data.CommandType.StoredProcedure;
